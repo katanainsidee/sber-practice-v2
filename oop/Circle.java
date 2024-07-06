11d12a2f1001c41f92690ee31a23d4dc9e58b7b6
katanainsidee <whoyuster@gmail.com>
2024-07-07 01:36:48 +0300

    2 задание
    Использовал модуль Math и его методы round, pow.

---
+++ oop/Circle.java
@@ -0,0 +1,32 @@
+public class Circle {
+    float radius;
+    String color;
+
+    public double area() {
+        return Math.round(Math.PI * Math.pow(this.radius, 2) * 100) / 100.0;
+    }
+
+    public double perimeter() {
+        return Math.round(2 * Math.PI * this.radius * 100) / 100.0;
+    }
+
+    public Circle(float radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public String toString() {
+        System.out.printf("%s, %s, %s, %s\n", this.radius, this.color, this.area(), this.perimeter());
+
+        return null;
+    }
+
+    static class Main {
+        public static void main(String[] args) {
+            Circle circle = new Circle(10, "red");
+            System.out.println(circle.area());
+            System.out.println(circle.perimeter());
+            circle.toString();
+        }
+    }
+}
